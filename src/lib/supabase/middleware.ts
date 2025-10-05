d1f25213b356647a90af0d42983188633c2bcf8b
Bill <billwang7599@gmail.com>
2025-10-05 18:20:28 +0800

    changed middleware to not redirect on root, and redirect correct link

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -41,13 +41,14 @@ export async function updateSession(request: NextRequest) {
 
     if (
         !user &&
+        request.nextUrl.pathname !== "/" &&
         !request.nextUrl.pathname.startsWith("/login") &&
         !request.nextUrl.pathname.startsWith("/auth") &&
         !request.nextUrl.pathname.startsWith("/error")
     ) {
         // no user, potentially respond by redirecting the user to the login page
         const url = request.nextUrl.clone();
-        url.pathname = "/login";
+        url.pathname = "/auth/login";
         return NextResponse.redirect(url);
     }
 
